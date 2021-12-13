7c740e41fbbf9d4a84ae5fd4e5eec4b40f5a8fec
l33th <l33th@protonmail.ch>
2021-12-13 08:05:17 +0200

    status code came back as 200

--- crypto-pull.py
+++ crypto-pull.py
@@ -1,7 +1,16 @@
+# Import modules and API key
+import requests
+import secrets
+
 # https://coinmarketcap.com/api/documentation/v1/
 
-# Import modules and API key
-
 # Test a request - basic
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map'
+headers = {
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': secrets.API_KEY,
+}
+
+r = requests.get(url, headers=headers)
 
 # Build a class to easily make REST API calls
